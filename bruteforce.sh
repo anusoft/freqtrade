448998c2f2adc9b316a5755a197d95a9a96869dc
Anu <anusoft@gmail.com>
2021-04-22 15:36:34 +0700

    add note book and iterations /loop

--- bruteforce.sh
+++ bruteforce.sh
@@ -65,40 +65,42 @@ fi
 
 echo "$(timestamp) Start Bruteforcing!!!"
 
-for coin in "${COINS[@]}" ; do
-    config="config-$coin-$STAKE_CURRENCY.json"
+for i in $(seq 1 $LOOP); do
+    for coin in "${COINS[@]}" ; do
+        config="config-$coin-$STAKE_CURRENCY.json"
 
-    for variablePair in "${ARRAY[@]}" ; do
-        HyperOpt="${variablePair%%:*}"
-        Strategy="${variablePair##*:}"
+        for variablePair in "${ARRAY[@]}" ; do
+            HyperOpt="${variablePair%%:*}"
+            Strategy="${variablePair##*:}"
 
-        for timeframe in ${TIMEFRAMES[@]}; do
-            for days in ${DAYSLIST[@]}; do
+            for timeframe in ${TIMEFRAMES[@]}; do
+                for days in ${DAYSLIST[@]}; do
 
-                echo "$(timestamp) coin=$coin strategy=$Strategy HYPEROPT_LOSS=$HYPEROPT_LOSS config=$config timeframe=$timeframe epoch=$EPOCH timerange=$timerange "
+                    echo "$(timestamp) coin=$coin strategy=$Strategy HYPEROPT_LOSS=$HYPEROPT_LOSS config=$config timeframe=$timeframe epoch=$EPOCH timerange=$timerange "
 
-                if [ "$(uname)" == "Darwin" ]; then
-                    timestart=$(date -v "-${days}d" +"%Y%m%d")
-                else
-                    timestart=$(date -d "${days} days ago" +"%Y%m%d")
-                fi
-                timeend=$(date +"%Y%m%d")
-                timerange="$timestart-$timeend"
+                    if [ "$(uname)" == "Darwin" ]; then
+                        timestart=$(date -v "-${days}d" +"%Y%m%d")
+                    else
+                        timestart=$(date -d "${days} days ago" +"%Y%m%d")
+                    fi
+                    timeend=$(date +"%Y%m%d")
+                    timerange="$timestart-$timeend"
 
-                rm -f .tmp.csv
-                rm -f user_data/hyperopt_results/.last_result.json
+                    rm -f .tmp.csv
+                    rm -f user_data/hyperopt_results/.last_result.json
 
-                if [ "$HyperOpt" = "MoniGoManiHyperStrategy" ]; then
-                    echo $(timestamp) $FREQTRADE_CMD hyperopt --spaces $SPACES --strategy $Strategy --hyperopt-loss $HYPEROPT_LOSS --config $config -i $timeframe -e $EPOCH --timerange $timerange
-                    $FREQTRADE_CMD hyperopt --spaces $SPACES --strategy $Strategy --hyperopt-loss $HYPEROPT_LOSS --config $config -i $timeframe -e $EPOCH --timerange $timerange
-                else
-                    echo $(timestamp) $FREQTRADE_CMD hyperopt --spaces $SPACES --hyperopt $HyperOpt --strategy $Strategy --hyperopt-loss $HYPEROPT_LOSS --config $config -i $timeframe -e $EPOCH --timerange $timerange
-                    $FREQTRADE_CMD hyperopt --spaces $SPACES --hyperopt $HyperOpt --strategy $Strategy --hyperopt-loss $HYPEROPT_LOSS --config $config -i $timeframe -e $EPOCH --timerange $timerange
-                fi
+                    if [ "$HyperOpt" = "MoniGoManiHyperStrategy" ]; then
+                        echo $(timestamp) $FREQTRADE_CMD hyperopt --spaces $SPACES --strategy $Strategy --hyperopt-loss $HYPEROPT_LOSS --config $config -i $timeframe -e $EPOCH --timerange $timerange
+                        $FREQTRADE_CMD hyperopt --spaces $SPACES --strategy $Strategy --hyperopt-loss $HYPEROPT_LOSS --config $config -i $timeframe -e $EPOCH --timerange $timerange
+                    else
+                        echo $(timestamp) $FREQTRADE_CMD hyperopt --spaces $SPACES --hyperopt $HyperOpt --strategy $Strategy --hyperopt-loss $HYPEROPT_LOSS --config $config -i $timeframe -e $EPOCH --timerange $timerange
+                        $FREQTRADE_CMD hyperopt --spaces $SPACES --hyperopt $HyperOpt --strategy $Strategy --hyperopt-loss $HYPEROPT_LOSS --config $config -i $timeframe -e $EPOCH --timerange $timerange
+                    fi
 
-                echo $(timestamp) python lastest_hyperopt_to_mongodb.py --config $config --timeframe $timeframe --epoch $EPOCH --spaces $SPACES --strategy $Strategy --hyperopt $HyperOpt --hyperopt-loss $HYPEROPT_LOSS --timerange $timerange --host $HOST --days $days --comment $COMMENT
-                python lastest_hyperopt_to_mongodb.py --config $config --timeframe $timeframe --epoch $EPOCH --spaces $SPACES --strategy $Strategy --hyperopt $HyperOpt --hyperopt-loss $HYPEROPT_LOSS --timerange $timerange --host $HOST --days $days --comment $COMMENT
+                    echo $(timestamp) python lastest_hyperopt_to_mongodb.py --config $config --timeframe $timeframe --epoch $EPOCH --spaces $SPACES --strategy $Strategy --hyperopt $HyperOpt --hyperopt-loss $HYPEROPT_LOSS --timerange $timerange --host $HOST --days $days --comment $COMMENT
+                    python lastest_hyperopt_to_mongodb.py --config $config --timeframe $timeframe --epoch $EPOCH --spaces $SPACES --strategy $Strategy --hyperopt $HyperOpt --hyperopt-loss $HYPEROPT_LOSS --timerange $timerange --host $HOST --days $days --comment $COMMENT
 
+                done
             done
         done
     done
